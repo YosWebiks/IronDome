1464e2d764d1ddfd345234bae7bcc214ecdb4b8a
Yosi <Yosi@YosiWebiks.com>
2024-08-06 17:57:34 +0300

    Add project files.

---
+++ IronDome/Models/Threat.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IronDome.Models
+{
+	public class Threat
+	{
+		[Key]
+		public int id { get; set; }
+
+		[NotMapped]
+		public int responseTime { get; set; }
+
+		public TerrorOrg Org { get; set; }
+
+		public ThreatAmmunition type { get; set; }
+
+		public bool isActive { get; set; }
+
+		public DateTime? fired_at { get; set; }
+	}
+}
