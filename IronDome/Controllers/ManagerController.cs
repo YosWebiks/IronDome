22cfe0d2a48b9c6a6e0b689c1dcac77e0a8f1b8d
Yosi <Yosi@YosiWebiks.com>
2024-08-07 12:39:08 +0300

    update amount works

--- IronDome/Controllers/ManagerController.cs
+++ IronDome/Controllers/ManagerController.cs
@@ -4,12 +4,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace IronDome.Controllers
 {
-	public class ManagerController : Controller
-	{
-		public IActionResult Index()
-		{
-			List<DefenceAmmunition> defences = Data.Get.DefenceAmmunitions.ToList();
-			return View(defences);
-		}
-	}
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<DefenceAmmunition> defences = Data.Get.DefenceAmmunitions.ToList();
+            return View(defences);
+        }
+
+        public IActionResult updateDefenceAmmiunition(int dfid, int amount)
+        {
+            DefenceAmmunition? da = Data.Get.DefenceAmmunitions.Find(dfid);
+            da.amount = amount;
+            Data.Get.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
 }
