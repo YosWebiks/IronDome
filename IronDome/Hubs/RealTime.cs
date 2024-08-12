a348725cf6b9d2e3214b867bca1e81895487ebb4
Yosi <Yosi@YosiWebiks.com>
2024-08-12 12:54:34 +0300

    socket support

--- IronDome/Hubs/RealTime.cs
+++ IronDome/Hubs/RealTime.cs
@@ -4,9 +4,16 @@ namespace IronDome.Hubs
 {
 	public class RealTime : Hub
 	{
-		public async Task SendMessage(string user, string message)
+
+		public async Task AttackAlert(int id, int rt, string name)
 		{
-			await Clients.All.SendAsync("ReceiveMessage", user, message);
+			await Clients.All.SendAsync("RedAlert", id, rt, name);
 		}
+
+
+		//public async Task SendMessage(string user, string message)
+		//{
+		//	await Clients.All.SendAsync("AttackAlert", user, message);
+		//}
 	}
 }
--- IronDome/wwwroot/js/realtime.js
+++ IronDome/wwwroot/js/realtime.js
@@ -1,1 +1,33 @@
-﻿
+﻿const connection = new signalR.HubConnectionBuilder().withUrl("/rt").build()
+
+connection.start()
+    .then(function () {
+        // do somthing once connected
+    })
+    .catch(function (err) {
+        return console.error(err.toString());
+    });
+
+
+// invoke launch
+function invokeLaunch(id, rt, name) {
+    connection.invoke("AttackAlert", id, rt, name)
+    console.log("I am inisde the launch invoke func")
+}
+
+// invoke inretcept
+
+// listen to lauch
+connection.on("RedAlert", function (id, rt, name) {
+    if (!window.location.href.includes("Deffence")) retrun;
+    const h1 = document.createElement("h1")
+    h1.style.color = "red"
+    h1.textContent = name + " has sent you a present! it'll arraive in " + rt + " seconds"
+    document.body.appendChild(h1)
+    setTimeout(() => {
+        document.body.removeChild(h1)
+    }, 1500)
+
+})
+
+// listen to intercept
